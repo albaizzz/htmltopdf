bb1114f4560fdac3ac1441322bf7c0d28510e885
albaizzz <albaidizzy@gmail.com>
2017-08-10 23:13:24 +0700

    create readme

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var request = require("request");
 
 request('http://localhost:3002/index', function(error, response, html) {
     var html1 = html;
-    pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
+    pdf.create(html, options).toFile('./sample-pdf.pdf', function(err, res) {
         if (err) return console.log(err);
         console.log(res); // { filename: '/app/businesscard.pdf' }
     });
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Html To PDF
+read webpage to stream and convert into PDF use html-pdf module
+
+$ cd static-content
+$ npm install
+$ node server.js
+
+$ cd..
+$ npm install -g html-pdf
+$ node index.js
+
