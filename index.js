5be9213395e36a9cc332eab5ab4e37d7379841bb
albaizzz <albaidizzy@gmail.com>
2017-08-10 22:52:26 +0700

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var pdf = require('html-pdf');
+// var html = fs.readFileSync('http://www.reservasi.com', 'utf8');
+var options = { format: 'Letter' };
+
+var http = require('http');
+var fs = require("fs");
+var request = require("request");
+
+request('http://localhost:3002/index', function(error, response, html) {
+    var html1 = html;
+    pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
+        if (err) return console.log(err);
+        console.log(res); // { filename: '/app/businesscard.pdf' }
+    });
+
+})
